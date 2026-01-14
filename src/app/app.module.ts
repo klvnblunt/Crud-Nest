4e763e3497ba9f11c100f3ae7b4e45bce5cec9a0
jonasklvn1234@gmail.com <jonasklvn1234@gmail.com>
2026-01-13 21:58:05 -0300

    Ajustando a entidade Recado com o typeORM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RecadosModule],
+  imports: [RecadosModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'nestuser',
+    database: 'nestdb',
+    password: '123456',
+    autoLoadEntities: true,
+    synchronize: true
+
+
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
